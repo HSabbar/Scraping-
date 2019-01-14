60238c80d12c9ccdd446ac48fb3daa302387c90f
HSabbar <36317041+HSabbar@users.noreply.github.com>
2019-01-14 16:36:58 +0000

    Add files via upload

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -4,7 +4,6 @@ import re
 import struct
 from selenium import webdriver
 
-
 #Pour choisir la langues 
 def download_page(url):
         browser = webdriver.Firefox()
@@ -14,11 +13,9 @@ def download_page(url):
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
-    with open('Resul.csv', 'w') as csvfile:
+    with open('Resultat.csv', 'w') as csvfile:
             spamwriter = csv.writer(csvfile, quotechar=' ', quoting=csv.QUOTE_ALL)
             spamwriter.writerow(['Title','Note', 'Avis', 'langues'])
-
-
 
     def start_requests(self):
         global url
@@ -30,37 +27,39 @@ class QuotesSpider(scrapy.Spider):
         driver = download_page(url)
 
         title = response.css('title::text')[0].extract()
-        note = response.css('.overallRating::text')[0].extract()
+        noteM = response.css('.overallRating::text')[0].extract()
         langues = "fr" #en it fr
         script_lang = "document.getElementById('filters_detail_language_filterLang_"+ langues +"').setAttribute('checked', 'checked')"
         
-        # Pour selectionner un langues c'est m'as pris un peu de temps parce que 
-        # il fallut que je rajoute dans HTML l'attribut ('checked'="checked") 
+        # Il faut que je rajoute dans HTML l'attribut ('checked'="checked") 
         # pour s'est faire j'ai rajoute de un code javaScripte pour que ca soit selectionner
        
         driver.execute_script(script_lang)
-
-        avis = driver.find_elements_by_class_name('listContainer.hide-more-mobile')
-        supreavis = ""
-        for av in avis:
-            supreavis += av.text.encode('utf-8') + '\n'
-
-        # driver.quit()
-
-        with open('Resul.csv', 'a') as csvfile:
-            spamwriter = csv.writer(csvfile)
-            spamwriter.writerow([title, note,supreavis, langues])
+        driver.find_element_by_css_selector(".taLnk.ulBlueLinks").click()
         
+        avis = driver.find_elements_by_class_name('listContainer.hide-more-mobile')
+        avv = avis[0].find_elements_by_tag_name("p")
 
-        # print("___________________ ", dir(langues))
-        # filename = 'file_Scgraper_%s.html' % page
-        # with open(filename, 'wb') as f:
-        #     # print("___________________",response.css('title::text')[0].extract())#extraire que de text
-        #     # print("___________________",response.css('title::text').extract_first())#extraire que de text sans les balises
-        #     # test = response.css('div.name.ui_header.h2')
-        #     print("___________________", response.css('div.ui_radio.item')[0].extract())
-        #     print("___________________",test[1].css("span.text::text").extract_first())#extraire div
-            
-        #     f.write(response.body)
+        avis_Brut = ""
+        for av in avv:
+            avis_Brut += av.text.encode('utf-8') + '\n\n'
 
-        # self.log('Saved file %s' % filename)
+
+        n = driver.find_elements_by_class_name("ui_bubble_rating")
+        not_avis = ""
+        not_SSavis = ""
+
+        classes = response.css('.ui_bubble_rating').xpath("@class").extract()
+
+        for cls in classes:
+            match = re.search(r'bubble_\d+\b', cls)
+            if match:
+                not_avis = str(format(match.group(0)))
+                i = int(re.findall("\d+", not_avis) [0])
+                not_SSavis += str(i) + "\n\n" 
+
+        driver.quit()
+
+        with open('Resultat.csv', 'a') as csvfile:
+            spamwriter = csv.writer(csvfile)
+            spamwriter.writerow([title, not_SSavis, avis_Brut, langues])
